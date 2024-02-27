34f6752a44d46d84e6d101144d9c86afd29a063c
Catisinha <luacoutinhor@gmail.com>
2024-02-27 09:38:56 -0300

    Alterada a classe Categoria, criada a classe Produto e instalado os pacotes de conexão e manipulação de dados em memória

--- Cozastore/Models/Categoria.cs
+++ Cozastore/Models/Categoria.cs
@@ -7,10 +7,25 @@ namespace Cozastore.Models;
 public class Categoria
 {
     [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
         
-        [Required(ErrorMessage = "Por favor, informe o Nome")]
-        [StringLength(30, ErrorMessage = "O Nome deve possuir no máximo 30 caracteres")]
-        public string Nome { get; set; }
+    [Required(ErrorMessage = "Por favor, informe o Nome")]
+    [StringLength(30, ErrorMessage = "O Nome deve possuir no máximo 30 caracteres")]
+    public string Nome { get; set; }
+
+    [StringLength(300)]
+    public string Foto { get; set; }
+
+    [Display(Name = "Exibir como filtro?")]
+    public bool Filtrar { get; set; }
+
+    [Display(Name = "Exibir como Banner?")]
+    public bool Banner { get; set; }
+
+    [Display(Name = "Categoria Mãe")]
+    public int? CategoriaMaeId { get; set; }
+    [ForeignKey("CategoriaMaeId")]
+    public Categoria CategoriaMae { get; set; }
+
 }
